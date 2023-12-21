2b0e4f43305fb652b96c6b133c40661893266f2b
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-21 16:28:27 -0300

    Finalizado tipos primitivos.

--- type-string.js
+++ type-string.js
@@ -30,3 +30,24 @@ console.log(cidade === inputMinusculo); // true
 
 const senha = "minhaSenha123"
 console.log(senha.length) // 13 caracteres
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+console.log(operacao);
+
+
+/**Que o computador consegue apenas armazenar valores em alguns tipos de variáveis, como números, texto e booleanos.
+Como declarar uma variável numérica, com tipo number, e associar um valor a ela, podendo ser um inteiro ou um valor de ponto flutuante.
+Como declarar uma variável de texto, conhecida como string, e colocar uma frase, número, pontuação ou uma mistura de todos esses dentro dela.
+Como declarar uma variável booleana e definir seu valor como verdadeiro (true) ou falso (false).
+Onde as booleanas mais aparecem, como resultados de comparações com === ou outros operadores de comparação como >=.**/
