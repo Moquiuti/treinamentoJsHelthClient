e6a389f0c8663b9a0c02dab7038d03f8547c14a3
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-22 16:17:37 -0300

    testando um template.

---
+++ template.js
@@ -0,0 +1,8 @@
+const nome = "Leandro";
+const idade = 34;
+const cidadeDeNascimento = "Campo Grande-MS";
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = "meu nome é ${ nome }, minha idade é ${ idade } e nasci na cidade de ${ cidadeDeNascimento }";
+
+console.log(apresentacao)
