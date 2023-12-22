2df5510d37dc4772bf587b1d50bc1f7e3449cd43
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-22 09:19:11 -0300

    Entendendo o conceito de Truthy e falsy e um pouco mais sobre null no Node.

---
+++ true-false.js
@@ -0,0 +1,8 @@
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log("Variável: "+minhaVar+" tipo da variável: "+typeof minhaVar) 
+console.log("Variável: "+varNull+" tipo da variável: "+typeof varNull) 
