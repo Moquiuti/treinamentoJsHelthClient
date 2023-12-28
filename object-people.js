54097511ed40e7a9768c384a2e32ec7fbb39e91a
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-28 14:21:15 -0300

    Adicionando e Criando parâmetros após a criação do objeto

--- object-people.js
+++ object-people.js
@@ -4,14 +4,21 @@ const client = {
     age: 34,
     cpf: "10293847561",
     email: "leandro@dominio.com",
-  };
-  
-  console.log(
+};
+
+console.log(
     `The name of client is ${client["firtsName"]} and has ${client["age"]} yeas old.`
-  );
-  
-  const chaves = ["firtsName", "age", "cpf", "email", "height"];
-  
-  chaves.forEach((chave) => {
+);
+
+const chaves = ["firtsName", "age", "cpf", "email", "height"];
+
+chaves.forEach((chave) => {
     console.log(`The key ${chave} has value ${client[chave]}`);
-  });
+});
+/**
+ * é possível criar propriedades ao objeto após o mesmo ter sido criado
+ */
+client.height = "1.76"
+chaves.forEach((chave) => {
+    console.log(`The key ${chave} has value ${client[chave]}`);
+});
