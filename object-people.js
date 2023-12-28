b96f7a9b3bbde0f92e385f9fe5c2aa76723c9096
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-28 15:22:04 -0300

    Manipulando objetos

--- object-people.js
+++ object-people.js
@@ -4,6 +4,7 @@ const client = {
     age: 34,
     cpf: "10293847561",
     email: "leandro@dominio.com",
+    telefone: ["+55 (67) 9 9999-9999", "+55 (67) 9 8888-7777"]
 };
 
 console.log(
@@ -15,6 +16,7 @@ const chaves = ["firtsName", "age", "cpf", "email", "height"];
 chaves.forEach((chave) => {
     console.log(`The key ${chave} has value ${client[chave]}`);
 });
+console.log("_____________")
 /**
  * é possível criar propriedades ao objeto após o mesmo ter sido criado
  */
@@ -22,9 +24,60 @@ client.height = "1.76"
 chaves.forEach((chave) => {
     console.log(`The key ${chave} has value ${client[chave]}`);
 });
+console.log("_____________")
 /**
  * Removendo uma propriedade
  */
 delete client.height
 
 console.log(client.height) //undefined
+
+/**
+ * Criando um objeto aninhado
+ */
+//client.endereco = {
+//    rua: "R. Joseph Climber",
+//    numero: 1337,
+//    apartamento: true,
+//    complemento: "ap 934",
+//};
+//
+//console.log(client)
+client.address = [
+    {
+        street: "R. Joseph Climber",
+        number: 1337,
+        apartment: true,
+        complement: "ap 934",
+        shipping: true,
+        business: true
+    },
+    {
+        street: "R. Pedre di Lari",
+        number: 188,
+        apartment: false,
+        complement: "Casa Térrea",
+        shipping: false,
+        business: false
+    },
+    {
+        street: "R. Marginal Luís Inácio Lula Ladrão",
+        number: 151,
+        apartment: false,
+        complement: "Carandiru",
+        shipping: false,
+        business: false
+    }
+];
+
+/**
+ * Apresentando um excelente benefício de trabalhar com uma lista de objetos por exemplo, onde
+ * pode-se aplicar filtros inteligentes para geração de relatório como no exemplo, onde só me interessa
+ * os endereço que são apartamentos....
+ */
+
+const listaApenasApartamentos = client.address.filter(
+    (address) => address.apartment === true
+);
+
+console.log(listaApenasApartamentos);
