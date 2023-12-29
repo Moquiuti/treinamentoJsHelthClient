c322af38c5c5dbc8f80bc9f4d144f3b43dc72f52
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-29 10:34:10 -0300

    Encontrar os clientes que moram em apartamentos, porém que não adicionaram complemento de endereço, utilizando hasownProperty

--- JSON/find.js
+++ JSON/find.js
@@ -8,7 +8,21 @@ const clients = require("./clients.json");
  * @returns 
  */
 const result = (lista, chave, valor) => lista.find((item) => item[chave].includes(valor));
+/**
+ * Encontrar os clientes que moram em apartamentos, porém que não adicionaram complemento de endereço
+ * @param {*} clientes 
+ * @returns 
+ */
+const filtrarApartamentoSemComplemento = (clientes) => {
+    return clientes.filter((cliente) => {
+        return (
+            cliente.endereco.apartamento &&
+            !cliente.endereco.hasOwnProperty("complemento")
+        );
+    });
+}
 
 //console.log(result(clients, "nome", "Kirby"));
-console.log(result(clients, "telefone", "1918820860"));
+//console.log(result(clients, "telefone", "1918820860"));
+console.log(filtrarApartamentoSemComplemento(clients));
 
