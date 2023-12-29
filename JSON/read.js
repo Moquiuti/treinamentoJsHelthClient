43ea724068fe99c080dd4ecaa21a19b5aaa72a5e
Leandro Moquiuti <leandro.moquiuti@gmail.com>
2023-12-29 09:19:55 -0300

    Iniciando práticas com JSON

---
+++ JSON/read.js
@@ -0,0 +1,9 @@
+const client = require("./client.json");
+
+/**
+ * Verificando o que foi rebcedido
+ * Que não é apenas uma string bem formatada, mas realmente um objeto
+ * 
+ */
+console.log(typeof client);
+console.log(client);
